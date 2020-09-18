35a1702d89eb4b16dedc2b4689d59b1a5043f34b
Roman Govor 850503 <roma-govor2013@ya.ru>
2020-09-18 22:12:29 +0300

    feat: my function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+        let strNum = n.toString();
+        if (strNum[0] === '-') {
+            strNum = strNum.slice(1);
+        }
+        const ReverseNum = +(strNum.split('').reverse().join(''));
+        return ReverseNum;
 }
